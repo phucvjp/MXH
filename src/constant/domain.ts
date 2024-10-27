a42c8c21ebfee147c1728a9a3710f224c76b41e9
phucvjp <169065491+phucvjp@users.noreply.github.com>
2024-10-27 19:07:26 +0700

    Update domain.ts

--- src/constant/domain.ts
+++ src/constant/domain.ts
@@ -3,11 +3,11 @@
 // export const WS_BACK_END: string = `ws://${BE_IP}:8080`;
 // export const NG_HEADER = {}
 
-export const BE_IP: string = "5be9-14-169-10-12";
+export const BE_IP: string = "cb0d-14-169-10-12";
 export const BACK_END: string = `https://${BE_IP}.ngrok-free.app`;
 export const WS_BACK_END: string = `wss://${BE_IP}.ngrok-free.app`;
 
 export const NG_HEADER = {
-  "Access-Control-Allow-Origin": "http://localhost:5173",
+  "Access-Control-Allow-Origin": "*",
   "ngrok-skip-browser-warning": "true",
 };
