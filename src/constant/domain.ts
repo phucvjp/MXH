360571572bb3c1ab86a3d83f016d30e35abb052e
phucvjp <169065491+phucvjp@users.noreply.github.com>
2024-10-29 16:35:20 +0700

    Update domain.ts

--- src/constant/domain.ts
+++ src/constant/domain.ts
@@ -1,13 +1,13 @@
-export const BE_IP:string = 'localhost';
-export const BACK_END:string  = `http://${BE_IP}:8080`;
-export const WS_BACK_END: string = `ws://${BE_IP}:8080`;
-export const NG_HEADER = {}
+// export const BE_IP:string = 'localhost';
+// export const BACK_END:string  = `http://${BE_IP}:8080`;
+// export const WS_BACK_END: string = `ws://${BE_IP}:8080`;
+// export const NG_HEADER = {}
 
-// export const BE_IP: string = "78f8-118-69-36-136";
-// export const BACK_END: string = `https://${BE_IP}.ngrok-free.app`;
-// export const WS_BACK_END: string = `wss://${BE_IP}.ngrok-free.app`;
+export const BE_IP: string = "aacf-2401-d800-b22-a3c7-f1b3-128a-7bb2-e1bb";
+export const BACK_END: string = `https://${BE_IP}.ngrok-free.app`;
+export const WS_BACK_END: string = `wss://${BE_IP}.ngrok-free.app`;
 
-// export const NG_HEADER = {
-//   "Access-Control-Allow-Origin": "*",
-//   "ngrok-skip-browser-warning": "true",
-// };
+export const NG_HEADER = {
+  "Access-Control-Allow-Origin": "*",
+  "ngrok-skip-browser-warning": "true",
+};
